d4e7bfc13906b467b1351474ebebc5848fa18760
Jameson sandler <108235294+J-sandler@users.noreply.github.com>
2023-01-11 23:11:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-const GenerationSize=800;
+//globals:
+var GenerationSize=180;
+var autoTrain=localStorage.getItem("trainState")?JSON.parse(localStorage.getItem("trainState")):false;
+const numTraffic=400;
+const learningRate=0.01;
+var mutationFactor=0.2;
+var recordModel=localStorage.getItem("recordState")?JSON.parse(localStorage.getItem("recordState")):false;
 const carCanvas=document.getElementById("carCanvas");
-carCanvas.width=300;
+carCanvas.width=350;
 
 const netCanvas=document.getElementById("netCanvas");
-netCanvas.width=450;
+netCanvas.width=550;
 
 const carCtx = carCanvas.getContext("2d");
 const netCtx = netCanvas.getContext("2d");
 
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
-const cars=createGeneration(GenerationSize);
+let cars=createGeneration(GenerationSize);
 let bestChild=cars[0]; //randomly initialize this child
 
 //compile next generation:
@@ -22,24 +28,26 @@ if (localStorage.getItem("bestChild")) {
     cars[i].mind=JSON.parse(
       localStorage.getItem("bestChild")
     );
-    NeuralNetwork.birth(cars[i].mind,0.1); //adjust mutation factor here
+    NeuralNetwork.birth(cars[i].mind,mutationFactor); //adjust mutation factor here
   }
 }
 
-const traffic=[
-  new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(3),-300,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(0),-100,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(1),-200,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(2),-500,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(3),-700,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(0),-700,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(2),-900,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(3),-1000,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(1),-1000,30,50,"DUMMY",2),
-  new Car(road.getLaneCenter(3),-1100,30,50,"DUMMY",2)  
-];
+if (recordModel) {
+	cars=[new Car(
+      road.getLaneCenter(1),
+      100,
+      30,
+      40,
+      "Record",
+      4
+    )];
+	bestChild=cars[0];
+	if(localStorage.getItem("bestChild")) localStorage.removeItem("bestChild");
+}
+
+const traffic=[];
+
+compileTraffic(traffic,numTraffic);
 
 animate();
 
@@ -58,6 +66,15 @@ function createGeneration(N) {
   return cars;
 }
 
+function compileTraffic(traffic,numCars) {
+    for (let i=0;i<numCars;i++) {
+        const randLane=Math.round(Math.random()*(3));//lane count -1
+        const randY=Math.round(Math.random()*-(135*numCars));
+		const trafficCar=new Car(road.getLaneCenter(randLane),randY,30,50,"DUMMY",2);
+        traffic.push(trafficCar);
+    }    
+}
+
 function preserve() {
   localStorage.setItem(
     "bestChild",
@@ -70,9 +87,22 @@ function destroy() {
 }
 
 function animate(time) {
-    for (let i=0;i<traffic.length;i++) {traffic[i].update(road.borders,[]);}
-    for (let i=0;i<cars.length;i++) cars[i].update(road.borders,traffic);
+    for (let i=0;i<traffic.length;i++) {
+		traffic[i].update(road.borders,[]);
+		if (autoTrain) {
+			traffic[i].maxSpeed=40;
+			traffic[i].acceleration=2;
+		}
+	}
+    for (let i=0;i<cars.length;i++) {
+		cars[i].update(road.borders,traffic);
+		if(autoTrain) {
+			cars[i].maxSpeed=60;
+			cars[i].acceleration=2;
+		}
+	}
 
+	//alternative reward would be implemented here:
     bestChild=cars.find(
       c=>c.y==Math.min(
         ...cars.map(
@@ -88,7 +118,7 @@ function animate(time) {
     carCtx.translate(0,-bestChild.y+carCanvas.height*0.7);
 
     road.draw(carCtx);
-    carCtx.globalAlpha=0.2;
+    carCtx.globalAlpha=0.1;
     for (let i=0;i<cars.length;i++) cars[i].draw(carCtx,"blue");
     carCtx.globalAlpha=1;
     for (let i=0;i<traffic.length;i++) {traffic[i].draw(carCtx,"green");}
@@ -96,7 +126,38 @@ function animate(time) {
     bestChild.draw(carCtx,"blue",true);
     carCtx.restore();
 
-    netCtx.lineDashOffset=-time*0.020;
+    netCtx.lineDashOffset=-time*0.04;
     Visualizer.drawNetwork(netCtx,bestChild.mind);
     requestAnimationFrame(animate);
+	if(time>=5000&&autoTrain) {
+
+		mutationFactor=(bestChild.damaged)?mutationFactor+learningRate:mutationFactor-learningRate;
+		mutationFactor=Math.abs(mutationFactor);
+
+		preserve();
+		location.reload();
+	}
+}
+
+function toggleAutoTrain() {
+	if (localStorage.getItem("trainState")) {
+		autoTrain=JSON.parse(localStorage.getItem("trainState"));
+		autoTrain=!autoTrain;
+		localStorage.removeItem("trainState");
+	} else {
+		autoTrain=!autoTrain;
+	}
+	localStorage.setItem("trainState",JSON.stringify(autoTrain));
+}
+
+function toggleRecord() {
+	if (localStorage.getItem("recordState")) {
+		recordModel=JSON.parse(localStorage.getItem("recordState"));
+		recordModel=!recordModel;
+		localStorage.removeItem("recordState");
+	} else {
+		recordModel=!recordModel;
+	}
+	localStorage.setItem("recordState",JSON.stringify(recordModel));
+	if (!recordModel) localStorage.setItem("bestChild",JSON.stringify(bestChild.mind));
 }
